ec9d19245b1a8be652e069768e36a6f9f3ce2c76
radjaho973 <adjaho.b@hotmail.fr>
2023-09-13 22:07:40 -0400

    Ajout du controller Home Modification visuelle du site

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,5 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// import './styles/app.css';
+import './styles/sass/app.scss';
