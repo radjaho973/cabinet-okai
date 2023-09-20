bb8416dc2d20ed02b607262eb4de8dde25daaddc
radjaho973 <adjaho.b@hotmail.fr>
2023-09-20 01:49:15 -0400

    UX/UI : Page Acceuil, Header, A propos et Expertise

---
+++ src/Controller/ExpertiseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExpertiseController extends AbstractController
+{
+    #[Route('/expertise', name: 'app_expertise')]
+    public function index(): Response
+    {
+        return $this->render('expertise/index.html.twig', [
+            'controller_name' => 'ExpertiseController',
+        ]);
+    }
+}
