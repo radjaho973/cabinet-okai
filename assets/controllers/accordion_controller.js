9403f1856f1c54fe29b7f736953960a87ad6b771
radjaho973 <adjaho.b@hotmail.fr>
2023-09-25 01:11:09 -0400

    UX/UI

--- assets/controllers/accordion_controller.js
+++ assets/controllers/accordion_controller.js
@@ -2,6 +2,26 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     connect() {
-        console.log(this)
+        
+            // Récupérez tous les éléments de classe "accordion-toggle"
+        const accordionToggles = document.querySelectorAll(".accordion-toggle");
+
+        // Parcourez tous les titres de l'accordéon et ajoutez un gestionnaire d'événements clic
+        accordionToggles.forEach((toggle) => {
+            toggle.addEventListener("click", function () {
+                // Récupérez le contenu de l'accordéon associé
+                const content = this.nextElementSibling;
+                
+                // Basculer la classe "active" sur le titre pour le style
+                this.classList.toggle("active");
+
+                // Vérifiez si le contenu est visible ou caché
+                if (content.style.display === "block") {
+                    content.classList.toggle("active"); // Cacher le contenu
+                } else {
+                    content.classList.toggle("active");  // Afficher le contenu
+                }
+            });
+        });
     }
 }
