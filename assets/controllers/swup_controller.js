87a44d35f348eadfecebdec554c581945806fdd9
Adjaho Barbier <adjaho.b@hotmail.fr>
2023-09-28 16:27:51 -0400

    UI/UX

--- assets/controllers/swup_controller.js
+++ assets/controllers/swup_controller.js
@@ -18,7 +18,49 @@ export default class extends Controller {
 
         swup.on('contentReplaced', function () {
             window.scrollTo(0, 0);
+            let titles = document.querySelectorAll("#hero-title")
+            ChangeTitles(titles)
         });
+        function ChangeTitles(titles) {
+            titles.forEach(title => {
+                
+                let pathname =window.location.pathname
+    
+                if (window.innerWidth <= 768) {
+    
+                    switch (pathname) {
+                        case "/expertise":
+                            title.innerHTML ="NOTRE EXPERTISE <br> <span class=\"lightblue\">VOS DÉCISIONS</span>"
+                            break;
+                        case "/urbanisme":
+                            title.innerHTML ="L’URBANISME SUR MESURE<br>NOUS CRÉONS,<br>VOUS RÉALISEZ !"
+                            break;
+                    
+                        default:
+                            break;
+                    }
+                }else{
+    
+                    switch (pathname) {
+                        case "/expertise":
+                            title.innerHTML ="ÉCLAIRER LES DÉCISIONS <br>DE DEMAIN <br>AVEC NOTRE EXPERTISE <br>D’AUJOURD’HUI"
+                            break;
+                        case "/urbanisme":
+                            title.innerHTML ="L’URBANISME SUR MESURE<br>NOUS ÉLABORONS,<br>VOUS CONCRÉTISEZ !"
+                            break;
+                    
+                        default:
+                            break;
+                    }
+                    
+                }
+                // if (window.innerWidth <= 768) {
+                //     title.innerHTML ="NOTRE EXPERTISE <br> <span class=\"lightblue\">VOS DÉCISIONS</span>"
+                // }else{
+                //     title.innerHTML ="ÉCLAIRER LES DÉCISIONS <br>DE DEMAIN <br>AVEC NOTRE EXPERTISE <br>D’AUJOURD’HUI"
+                // }
+            })
+        }
     }
 
     disconnect() {
