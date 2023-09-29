1c76de14a085419cb4faf5324d09fccf8301549b
Adjaho Barbier <adjaho.b@hotmail.fr>
2023-09-29 14:12:12 -0400

    FEATURES : Mail Asynchrone

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,43 +3,34 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use App\Message\ContactMail;
 use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, MailerInterface $mailer): Response
+    public function index(Request $request, MessageBusInterface $bus): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             
-            $email = (new TemplatedEmail())
-            ->from('adresse@cabinet-okai.com')
-            ->to('hello@studio-okai.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Nouvelle demande de contact')
-            // ->text('Sending emails is fun again!')
-            ->htmlTemplate('mail/contact_mail.html.twig')
-            ->context([
+            $mailContent = [
                 'mail' => $form->get('email')->getData(),
                 'name' => $form->get('name')->getData(),
                 "lastname" => $form->get("lastname")->getData(),
                 "phone" => $form->get('phone')->getData(),
                 "message" => $form->get('message')->getData()
-            ]);
-
-        $mailer->send($email);
+            ];
+            $bus->dispatch(New ContactMail($mailContent));
 
             return $this->render('contact/index.html.twig', [
                 'form' => $form,
